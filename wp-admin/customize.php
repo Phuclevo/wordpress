a2b2c34a8cf209d7b2f21124c173171d8ed903ce
NguyenMinh-Hieu <85159304+NguyenMinh-Hieu@users.noreply.github.com>
2022-07-21 20:07:49 +0700

    web

---
+++ wp-admin/customize.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://phuclevo.github.io/wordpress/wp-login.php?redirect_to=https%3A%2F%2Fphuclevo.github.io%2Fwordpress%2Fwp-admin%2Fcustomize.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://phuclevo.github.io/wordpress/wp-login.php?redirect_to=https%3A%2F%2Fphuclevo.github.io%2Fwordpress%2Fwp-admin%2Fcustomize.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://phuclevo.github.io/wordpress/wp-login.php?redirect_to=https%3A%2F%2Fphuclevo.github.io%2Fwordpress%2Fwp-admin%2Fcustomize.php&reauth=1">https://phuclevo.github.io/wordpress/wp-login.php?redirect_to=https%3A%2F%2Fphuclevo.github.io%2Fwordpress%2Fwp-admin%2Fcustomize.php&reauth=1</a></p>
+	</body>
+</html>
